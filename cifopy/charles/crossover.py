a9e9486843eb3bc71995e5468b0af95485c7900f
LAPTOP-TIQN4STJ\gabri <gabriele.morina96@gmaill.com>
2021-05-08 17:01:00 +0100

    Added ordered crossover (OX) and uniform order crossover (UOX)

--- cifopy/charles/crossover.py
+++ cifopy/charles/crossover.py
@@ -1,4 +1,6 @@
-from random import randint, uniform
+from random import randint, uniform, sample
+import random
+import numpy as np
 
 
 def template_co(p1, p2):
@@ -12,7 +14,7 @@ def template_co(p1, p2):
         [type]: [description]
     """
 
-    return offspring1, offspring2
+    #return offspring1, offspring2
 
 def single_point_co(p1, p2):
     """Implementation of single point crossover.
@@ -68,10 +70,116 @@ def arithmetic_co(p1, p2):
         offspring2[i] = p2[i] * alpha + (1 - alpha) * p1[i]
 
     return offspring1, offspring2
+
+
+def pmx_co(p1, p2):
+    # Sample 2 random co points
+    co_points = sample(range(len(p1)), 2)
+    co_points.sort()
+
+    def PMX(x, y):
+        # Create placeholder for offspring
+        o = [None] * len(x)
+
+        # Copy co segment into offspring
+        o[co_points[0]:co_points[1]] = x[co_points[0]:co_points[1]]
+
+        # Find set of values not in offspring from co segment in P2
+        z = set(y[co_points[0]:co_points[1]]) - set(x[co_points[0]:co_points[1]])
+
+        # Map values in set to corresponding position in offspring
+        for i in z:
+            temp = i
+            index = y.index(x[y.index(temp)])
+            while o[index] != None:
+                temp = index
+                index = y.index(x[temp])
+            o[index] = i
+
+        while None in o:
+            index = o.index(None)
+            o[index] = y[index]
+        return o
+
+    o1, o2 = (
+        PMX(p1, p2),
+        PMX(p2, p1)
+    )
+
+    return o1, o2
     
+def ordered_co(p1, p2):
+    """Implementation of ordered crossover
+
+    Args:
+        p1 (Individual): First parent for crossover.
+        p2 (Individual): Second parent for crossover.
+
+    Returns:
+        Individuals: Two offspring, resulting from the crossover.
+    """
+    #sample 2 random co points
+    co_points = sample(range(len(p1)), 2)
+    co_points.sort()
+
+    def OX(x,y):
+        offspring = [None] * len(x)
+        #copy co segment into offspring
+        offspring[co_points[0]:co_points[1]] = x[co_points[0]:co_points[1]]
+
+        #fill in remaining values
+        temp = co_points[1]
+        for i in range(len(y)):
+            if (y[(co_points[1] +  i )%len(y)] not in x[co_points[0]:co_points[1]]):
+                offspring[temp] = y[(co_points[1] +  i )%len(y)]
+                temp = (temp +1)%len(offspring)
+
+        return offspring
+
+    #call the function twice with parents reversedd
+    offspring1, offspring2 = (OX(p1,p2), OX(p2,p1))
+    return offspring1, offspring2
+
+def uniform_order_co(p1, p2):
+    """Implementation of uniform order crossover
+
+    Args:
+        p1 (Individual): First parent for crossover.
+        p2 (Individual): Second parent for crossover.
+
+    Returns:
+        Individuals: Two offspring, resulting from the crossover.
+    """
+    #binary string of the same length as parents
+    mask = np.random.randint(2, size=len(p1))
+
+    offspring1 = [None] * len(p1)
+    offspring2 = [None] * len(p2)
+
+    #copying the genes from the parents to the offsprings according to the generated mask
+    for i in range(len(mask)):
+        if mask[i]:
+            offspring1[i] = p1[i]
+        else:
+            offspring2[i]=p2[i]
+
+    def fill_remaining(offspring, parent):
+        temp = 0
+        for i in range(len(offspring)):
+            if offspring[i] is None:
+                while parent[temp] in offspring:
+                    temp += 1
+                offspring[i] = parent[temp]
+        return offspring
+
+    # call the function twice with parents reversed
+    offspring1, offspring2 = (fill_remaining(offspring1, p2), fill_remaining(offspring2, p1))
+    return offspring1, offspring2
+
+
 
 if __name__ == '__main__':
-    p1 = [0.1,0.2,0.6,0.4,0.3,0.5,0.7]
-    p2 = [0.7,0.5,0.2,0.3,0.6,0.4,0.1]
+    p1 = [1,2,6,4,3,5,7]
+    p2 = [7,5,2,3,6,4,1]
 
-    print(arithmetic_co(p1, p2))
+    print(uniform_order_co(p1, p2))
