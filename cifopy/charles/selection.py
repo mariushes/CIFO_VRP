9addd218a5dd3558fd8c4bb6183159a2bf65f1f5
pire-TO <66904794+pire-TO@users.noreply.github.com>
2021-05-19 16:27:26 +0100

    This min seems better
    What I did is to subtract from the max fitness the fitness of each individual. Then I added a quantity so that the least likely individual will have the same probability as it would have in a max problem with the same fitness vector. I debugged it and it does what I intended. I have concerns instead on the overall performance, since it does not seem to improve from initial point (but maybe we need to fine tune parameters first?)

--- cifopy/charles/selection.py
+++ cifopy/charles/selection.py
@@ -1,4 +1,4 @@
-from random import uniform, sample
+from random import uniform, sample, choice
 from operator import attrgetter
 
 mo_functions = None
@@ -25,7 +25,31 @@ def fps(population):
             if position > spin:
                 return individual
     elif population.optim == "min":
-        raise NotImplementedError
+        pop_fitness = [i.fitness for i in population]
+        M, m = max(pop_fitness), min(pop_fitness)
+        #min_prob is the probability of the least likely individual in a max instance with same fitness
+        min_prob = m/sum(pop_fitness)
+        n = len(population)
+        #covering the (extremely unlikely) case where all indiv have the same fitness, that otherwise would bring
+        # a division by zero
+        if n*min_prob == 1:
+            return choice(population.individuals)
+        rev_fitness = [M-x for x in pop_fitness]
+        to_add = min_prob*sum(rev_fitness)/(1-n*min_prob)
+        #the new vector from which compute probabilities
+        rev_fitness[:] = [x+to_add for x in rev_fitness]
+         # Sum total fitnesses
+        total_fitness = sum(rev_fitness)
+        # Get a 'position' on the wheel
+        spin = uniform(0, total_fitness)
+        position = 0
+        i = 0
+        # Find individual in the position of the spin
+        for individual in population:
+            position += rev_fitness[i]
+            if position > spin:
+                return individual
+            i += 1
 
     else:
         raise Exception("No optimiziation specified (min or max).")
@@ -40,3 +64,4 @@ def tournament(population, size=20):
         return min(tournament, key=attrgetter("fitness"))
     else:
         raise Exception("No optimiziation specified (min or max).")
+
