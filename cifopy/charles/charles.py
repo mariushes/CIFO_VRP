ee96696184345befe250c99d3877af778132aa75
Marius Hessenthaler <mhessent@mail.uni-mannheim.de>
2021-05-28 20:37:05 +0100

    Clean codes and comments in charles and mutation

--- cifopy/charles/charles.py
+++ cifopy/charles/charles.py
@@ -28,7 +28,7 @@ class Individual:
             self.representation = representation
         self.fitness = self.evaluate()
         
-        #for multi-objective optimization
+        #for multi-objective optimization. Calculate second fitness function if it is monkeypatched
         try:
             self.fitness2 = self.evaluate2()
         except:
@@ -124,11 +124,12 @@ class Population:
                     elif self.optim == "min":
                         elite = deepcopy(min(self.individuals, key=attrgetter("fitness")))
             
+            # caluclate the variance for the entire population to lock it and check for premature convergence
             fitness = [i.fitness for i in self]
             var = np.var(fitness)
             
             if prem:
-                #Check if it's reached premature convergence 
+                #Check if it's reached premature convergence criteria by variance ratio
                 if var/self.initial_var<0.05:
                     #print("Premature convergence! Reshuffling the deck")
                     self.reshuffle()
@@ -153,9 +154,15 @@ class Population:
                 new_pop.append(Individual(representation=offspring1))
                 if len(new_pop) < self.size:
                     new_pop.append(Individual(representation=offspring2))
+
+
             #keep best individual if elitism==True
             if elitism == True:
                 if select == multi_objective_dominant:
+                    # for multi objective elitism we cannot just compute the max and the least
+                    # before creating the new population we already saved all the pareto efficient as the elites
+                    # here we replace any dominated individuals to by the elites
+
                     # calculate the pareto efficient (non-dominated) individuals
                     costs = [[indiv.fitness, indiv.fitness2] for indiv in self]
                     costs = np.array(costs)
@@ -165,7 +172,7 @@ class Population:
                         # if there is no elites left break the loop
                         if not elites:
                             break
-                        # remove an element from the elites list and replace a non-pareto (dominated) inidividual by it
+                        # get an element from the elites list and replace a non-pareto (dominated) inidividual by it
                         if not pareto:
                             self[i] = elites.pop()
                 else:
@@ -185,6 +192,7 @@ class Population:
 
             self.individuals = new_pop
 
+            # reset the pareto flags that are saved for the multi-objective selection for the previous population
             self.pareto_flags = None
             
             # if multi dominant objective is selection, save for indiv if it is pareto efficient which is needed for logging
@@ -194,6 +202,7 @@ class Population:
                 costs = np.array(costs)
                 pareto_list = is_pareto_efficient(costs, self.optim).tolist()
 
+                # save the pareto booleans to the corresponding individuals 
                 for i, pareto in enumerate(pareto_list):
                     self[i].pareto = pareto
 
@@ -235,10 +244,11 @@ class Population:
         elif self.optim == "min":
             new_pop.append(deepcopy(min(self.individuals, key=attrgetter("fitness"))))
 
-
+        # append the half population that will be kept
         keep = sample(range(n), k=n//2)
         for i in keep:
             new_pop.append(deepcopy(self.individuals[i])) #to test, not sure
+        # generate new random indivs until the size is reached
         while len(new_pop)<n:
             new_pop.append(Individual(
                     size=self.sol_size,
@@ -246,6 +256,7 @@ class Population:
                     valid_set=self.valid_set))
 
         self.individuals = new_pop
+
     #log the results
     def log(self, select, crossover, mutate, gens, co_p, mu_p, elitism, prem, var, log_representation = False):
         
@@ -257,7 +268,7 @@ class Population:
         with open(dir_name + "/run" + f'-{self.timestamp}.csv', 'a', newline='') as file:
             writer = csv.writer(file)
             for i in self:
-                # we write this later
+                # if clauses that log different things depening on the settings and type of problem
                 if log_representation:
                     if select == multi_objective_dominant:
                         writer.writerow([self.gen, i.representation, i.fitness, i.fitness2, i.pareto, var])
@@ -265,6 +276,7 @@ class Population:
                         writer.writerow([self.gen, i.representation, i.fitness,var])
                 else:
                     if select == multi_objective_dominant:
+                        #sometimes there is an error with the pareto values, then the logging is skipped
                         try:
                             writer.writerow([self.gen, i.fitness, i.fitness2, i.pareto, var])
                         except:
--- cifopy/charles/mutation.py
+++ cifopy/charles/mutation.py
@@ -61,17 +61,16 @@ def swap_sequence_mutation(individual):
     # get two points as start points for the two sequences
     seq_points = sample(range(len(individual)), 2)
 
-    # sort them 
+     # sort them 
     seq_points.sort()
     # the maximum sequence length is the minimum from the distance of the first to the second sequence point and the second to the end.
     max_len = min([seq_points[1] - seq_points[0] , len(i) - seq_points[1]])
     max_len = max([max_len,0])
+
     #sample the sequence length
     seq_len = sample(range(1,max_len+1), 1)[0]
 
-    # sort them 
-
-
+    # swap the sequences
     i[seq_points[0]:seq_points[0]+seq_len], i[seq_points[1]:seq_points[1]+seq_len] = i[seq_points[1]:seq_points[1]+seq_len], i[seq_points[0]:seq_points[0]+seq_len]
 
     return i
@@ -94,8 +93,8 @@ def centre_inverse_mutation(individual):
 def throas_mutation(individual):
     """
     https://arxiv.org/pdf/1203.3099.pdf
-    We construct a sequence of three genes: the first is selected randomly and the two
-    others are those two successors. Then, the last becomes the first of the sequence, the second becomes last and the first becomes the second in the sequence.
+    "We construct a sequence of three genes: the first is selected randomly and the two
+    others are those two successors. Then, the last becomes the first of the sequence, the second becomes last and the first becomes the second in the sequence."
     """
     
     i = individual
@@ -107,7 +106,11 @@ def throas_mutation(individual):
     return i
 
 def partial_shuffle_mutation(individual):
-    beta = 0.8
+    """
+    https://arxiv.org/pdf/1203.3099.pdf
+    Select each gene with a probability of beta and swap it with another random gene
+    """
+    beta = 0.1
     i = individual
     for k in range(len(individual)):
         if random() > beta:
@@ -121,31 +124,41 @@ def cheapest_insertion_mutation(individual):
     """
     Choose a random location in the individual and removes it. Inserts the location at the cheapest position.
     Not really a random mutation since it tries to optimize fitness by using the TSP heuristic.
-    TODO: Finish
     """
-    i = individual
+
+    # It needs the distance matrix and home to be monkey patched before
     if dm == None:
         raise Exception("You need to monkey patch the distance matrix.")
-    
     if home == None:
         raise Exception("You need to monkey patch the home.")
+
+    i = individual
+
+    # randomly selecte element to be reinserted and remove it
     insert_point = sample(range(len(i)), 1)
     insert = i.pop(insert_point[0])
 
+    # add home location to be able to calculate distance to home
     i.append(home)
 
     # Calculate cheapest detour
     cheapest_detour = 9999999
     cheapest_detour_pos = 0
     for j in range(len(i)):
+        # calculate detour when inserting between node j and previous node
         normal_length = dm[i[j-1]][i[j]]
         insert_length = dm[i[j-1]][insert] + dm[insert][i[j]]
         detour = insert_length - normal_length
+
+        # save detour position and value when cheaper than previous
         if detour < cheapest_detour:
             cheapest_detour = detour
             cheapest_detour_pos = j
-        
+    
+    # insert at cheapest detour
     i.insert(cheapest_detour_pos, insert)
+
+    # remove home again
     if i.pop() != home:
         raise Exception("Last Element not home.")
 
@@ -163,22 +176,3 @@ def inversion_mutation(individual):
 
     return i
     
-if __name__ == '__main__':
-    i1 = [1,2,3,4,5,6]
-    distance_matrix = [
-    [0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972],
-    [2451, 0, 1745, 1524, 831, 1240, 959, 2596, 403, 1589, 1374, 357, 579],
-    [713, 1745, 0, 355, 920, 803, 1737, 851, 1858, 262, 940, 1453, 1260],
-    [1018, 1524, 355, 0, 700, 862, 1395, 1123, 1584, 466, 1056, 1280, 987],
-    [1631, 831, 920, 700, 0, 663, 1021, 1769, 949, 796, 879, 586, 371],
-    [1374, 1240, 803, 862, 663, 0, 1681, 1551, 1765, 547, 225, 887, 999],
-    [2408, 959, 1737, 1395, 1021, 1681, 0, 2493, 678, 1724, 1891, 1114, 701],
-    [213, 2596, 851, 1123, 1769, 1551, 2493, 0, 2699, 1038, 1605, 2300, 2099],
-    [2571, 403, 1858, 1584, 949, 1765, 678, 2699, 0, 1744, 1645, 653, 600],
-    [875, 1589, 262, 466, 796, 547, 1724, 1038, 1744, 0, 679, 1272, 1162],
-    [1420, 1374, 940, 1056, 879, 225, 1891, 1605, 1645, 679, 0, 1017, 1200],
-    [2145, 357, 1453, 1280, 586, 887, 1114, 2300, 653, 1272, 1017, 0, 504],
-    [1972, 579, 1260, 987, 371, 999, 701, 2099, 600, 1162, 1200, 504, 0],
-]
-    home = 0
-    print(cheapest_insertion_mutation(i1))
